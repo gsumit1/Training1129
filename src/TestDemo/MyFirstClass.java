c6f6c7014137324e8ed45d68d8515844da86abf4
riyaanghosh <riyaanghosh@192.168.1.28>
2021-11-29 09:12:36 +0530

    added sum code

--- src/TestDemo/MyFirstClass.java
+++ src/TestDemo/MyFirstClass.java
@@ -1,5 +1,16 @@
 package TestDemo;
 
 public class MyFirstClass {
+	
+	public static void main(String arg[]) {
+		int a=5;
+		int b=6;
+		int c=0;
+		c=a+b;
+		
+		System.out.println(c);
+		
+		
+	}
 
 }
